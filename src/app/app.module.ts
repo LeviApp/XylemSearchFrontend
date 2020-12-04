ee1981099fc2d96147e6a1927d7822ee1411caf0
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2020-12-03 23:57:58 -0700

    added new search results component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { SavedComponent } from './saved/saved.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    SavedComponent
+    SavedComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
