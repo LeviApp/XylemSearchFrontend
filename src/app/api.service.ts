c5b59762112553aa0b4b54c9017096140b9fd7b2
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2021-01-08 23:11:10 -0700

    getting JSON data using Chrome Extension

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -9,18 +9,15 @@ import { catchError } from 'rxjs/operators';
 })
 export class ApiService {
   private _searchapi: string = ""
+  public results = []
+
   constructor(private http: HttpClient) { }
 
   plantSearch(val): Observable<any> {
-    let params = new HttpParams()
-    .set("token", "zgXCcdZKTKqpECf1MxQaBJ9zTO39Wdaln6kB88ffzcQ")
-    .set("q", "cactus");
-    this._searchapi = `https://trefle.io/api/v1/plants/search?token=zgXCcdZKTKqpECf1MxQaBJ9zTO39Wdaln6kB88ffzcQ&q=cactus`
-    // this._searchapi = "https://quotes-4-life.herokuapp.com/home"
-    // this._searchapi = `https://www.omdbapi.com/?s=${val}&type=movie&page=1&apikey=e7fa079b`
 
-    console.log('You searched for a plant!', val)
-    return this.http.get<any>(this._searchapi, {params})
+    this._searchapi = `http://trefle.io/api/v1/plants/search?token=zgXCcdZKTKqpECf1MxQaBJ9zTO39Wdaln6kB88ffzcQ&q=${val}`
+
+    return this.http.get<any>(this._searchapi)
   }
 
   handleError(error) {
